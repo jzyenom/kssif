2251969d25d9adb201bb4ef2f8fbf58557095d5e
japheth <jzyenom@gmail.com>
2024-12-08 02:17:42 +0100

    Seo-op

--- app/page.js
+++ app/page.js
@@ -1,16 +1,18 @@
-import Head from "next/head";
+'use client'
+import  {NextSeo}  from "next-seo";
 
 export default function NotFound() {
   return (
     <>
-      <Head>
-        <title>Kingdom Star Sports International Foundation</title>
-        <meta
-          name="description"
-          content="Welcome to Kingdom Star Sports International Foundation, where faith meets sports and purpose meets action."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <NextSeo
+        title="KSSIF - KINGDOM STARS SPORT INTERNATIONAL FOUNDATION"
+        description="KSSIF - 404-Page"
+        content="Welcome to Kingdom Star Sports International Foundation, where faith meets sports and purpose meets action."
+        canonical="https://www.kssif.org"
+        openGraph={{ 
+          url: "https://www.kssif.org",
+        }}
+      />
       <div className="flex flex-col justify-center mx-auto mt-20 text-center max-w-2xl">
         {/* Page Header */}
         <h1 className="text-2xl mt-10 font-bold tracking-tight text-black md:text-5xl mb-4">
@@ -30,6 +32,7 @@ export default function NotFound() {
             />
           </div>
         </div>
+
         {/* 404 Message */}
         <h2 className="text-3xl font-bold tracking-tight text-black md:text-5xl mt-10">
           404 – Unavailable
