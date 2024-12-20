6ac486b1f667d996e2071066da6dd5ca32f5ea46
japheth <jzyenom@gmail.com>
2024-12-20 10:27:45 +0100

    removed Svg components

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import React from 'react'
-import Nav from "@/components/Nav"; 
-import Hero from "@/components/Hero"; 
+import React from "react";
+import Nav from "@/components/Nav";
+import Hero from "@/components/Hero";
 import Partners from "@/components/Partners";
 import UpcomingEvents from "@/components/UpcomingEvents";
-import LatestNews from "@/components/LatestNews"; 
-import NewsLetter from "@/components/NewsLetter"; 
-import Footer from "@/components/Footer"; 
+import LatestNews from "@/components/LatestNews";
+import NewsLetter from "@/components/NewsLetter";
+import Footer from "@/components/Footer";
 
 const Home: React.FC = () => {
   return (
